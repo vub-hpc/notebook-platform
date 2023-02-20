ad6b3920bd230467f0e2921f8ebace571a46c310
Alex Domingo <alex.domingo.toro@vub.be>
2023-02-20 12:15:25 +0100

    update configuration of JupyterHub to rootless and userless container with tight SSH connection

--- jupyterhub/host/usr/local/bin/jupyterhub-init.sh
+++ jupyterhub/host/usr/local/bin/jupyterhub-init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2022 Vrije Universiteit Brussel
+# Copyright 2023 Vrije Universiteit Brussel
 #
 # This file is part of notebook-platform,
 # originally created by the HPC team of Vrij Universiteit Brussel (http://hpc.vub.be),
@@ -37,8 +37,8 @@
 #
 
 JH_CONTAINER_NAME=${1:-jupyterhub}
-JH_CONTAINER_TAG="2.0"
-JH_CONTAINER_SRC="localhost/jupyterhub-rootless:$JH_CONTAINER_TAG"
+JH_CONTAINER_TAG="2.3"
+JH_CONTAINER_SRC="localhost/jupyterhub-moss:$JH_CONTAINER_TAG"
 
 # Custom runtime directory
 pid_file=""
@@ -51,16 +51,18 @@ if [ -n "$JH_CONTAINER_RUNDIR" ]; then
 fi
 
 # Container port
-# Forward container port 8000 (web) and port 8081 (api)
+# and forwards port 80 to 8000 (web) and port 8081 to 8081 (api)
 podman_port_web="-p 8000:8000/tcp"
 podman_port_api="-p 8081:8081/tcp"
 podman_port="$podman_port_web $podman_port_api"
 
-# VSC user home directories
-vsc_institutes="brussel"
-vsc_home_mounts=""
-for institute in $vsc_institutes; do
-    vsc_home_mounts="$vsc_home_mounts -v /user/$institute:/user/$institute"
+# Bind mounts of JupyterHub operator
+bind_folders=".ssh .ssl .config"
+podman_homebinds=""
+for folder in $bind_folders; do
+    if [ -d "$HOME/$folder" ]; then
+        podman_homebinds="$podman_homebinds -v $HOME/$folder:$HOME/$folder"
+    fi
 done
 
 # Run JupyterHub container
@@ -77,16 +79,14 @@ else
         -d                                  # run in detached mode
         $podman_pid $podman_cid             # define runtime files
         $podman_port                        # define container network settings
-        $vsc_home_mounts                    # bind mount VSC_HOME directories
         --log-driver=journald               # use journald to manage container logs
         -v /dev/log:/dev/log                # bind mount /dev/log to make container logs available in host
-        -v /etc/pki:/etc/pki                # bind directory with certificates
-        -v /etc/jupyterhub:/etc/jupyterhub  # bind directory with configuration files
-        --group-add=etcconf                 # add secondary groups from host system
+        $podman_homebinds                   # bind mount of directories in operator home
+        --userns=keep-id                    # keep UID of operator inside the container
         --name=$JH_CONTAINER_NAME           # define container name
         $JH_CONTAINER_SRC                   # source of container image
-        jupyterhub                               # COMMAND #
-        -f /etc/jupyterhub/jupyterhub_config.py  # configuration file
+        jupyterhub                          # COMMAND #
+        -f ~/.config/jupyterhub_config.py   # configuration file
     )
     podman_cmd="run ${podman_run_args[@]}"
 fi
